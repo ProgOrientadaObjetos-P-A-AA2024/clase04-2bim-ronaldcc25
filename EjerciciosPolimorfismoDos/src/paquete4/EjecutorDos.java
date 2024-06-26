f38bc5cd69e28ad643304fae4c25cd1910c80b0f
ronaldcc25 <racalderon13@utpl.edu.ec>
2024-06-26 16:50:40 -0500

    falta ejecutor 2

--- EjerciciosPolimorfismoDos/src/paquete4/EjecutorDos.java
+++ EjerciciosPolimorfismoDos/src/paquete4/EjecutorDos.java
@@ -6,28 +6,36 @@
 package paquete4;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import paquete2.Arriendo;
 import paquete3.ArriendoLocalComercial;
 import paquete3.ArriendoLocalComida;
 import paquete3.ArriendoLocalSesiones;
 
-public class Ejecutor {
+public class EjecutorDos {
 
     public static void main(String[] args) {
 
+        Scanner leer = new Scanner(System.in);
+        int opc;
         ArrayList<Arriendo> listaArriendos = new ArrayList<>();
 
-        ArriendoLocalComida arriendoComida = new ArriendoLocalComida(
-                "Christian Shepherd", 300);
-        arriendoComida.establecerIva(10); // en porcentaje
-        arriendoComida.establecerValorAgua(20.2); // en $
-        arriendoComida.establecerValorLuz(40.2); // en $
-
-        ArriendoLocalComercial arriendoComercial = new ArriendoLocalComercial(
+        for (int i = 0; i < 10; i++) {
+            ArriendoLocalComida arriendoComida = new ArriendoLocalComida(
+                    "Christian Shepherd", 300);
+            arriendoComida.establecerIva(10); // en porcentaje
+            arriendoComida.establecerValorAgua(20.2); // en $
+            arriendoComida.establecerValorLuz(40.2); // en $
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            ArriendoLocalComercial arriendoComercial = new ArriendoLocalComercial(
                 "Andrew Schroeder", 400);
         arriendoComercial.establecerValorAdicionalFijo(100); // en $
-
-        ArriendoLocalSesiones arriendoSesiones = new ArriendoLocalSesiones(
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            ArriendoLocalSesiones arriendoSesiones = new ArriendoLocalSesiones(
                 "Angela Watson", 350);
         arriendoSesiones.establecerValorSillas(10); // en $
         arriendoSesiones.establecerValorAmplificacion(20); // en $
@@ -35,6 +43,13 @@ public class Ejecutor {
         listaArriendos.add(arriendoComida);
         listaArriendos.add(arriendoComercial);
         listaArriendos.add(arriendoSesiones);
+        }
+
+        
+
+        
+
+        
 
         for (int i = 0; i < listaArriendos.size(); i++) {
             listaArriendos.get(i).establecerArriendoMensual();
@@ -47,4 +62,5 @@ public class Ejecutor {
         System.out.println(centro);
 
     }
+   
 }
